4c0df2763f383b591a45a6881beb3c983bf2889e
anshid <anshid@metrictreelabs.com>
2024-03-13 14:56:24 +0530

    datetime.utcnow to chenged to datetime.now(timezone.utc)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 from connection import ConnectionManager
 from database import chat_room, chat_group, chat_message
-from datetime import datetime
+from datetime import datetime, timezone
 from fastapi import FastAPI, HTTPException, Body, WebSocket
 from typing import List
 from typing_extensions import Annotated
@@ -32,14 +32,14 @@ async def create_group(
         raise HTTPException(400, "This name already exists")
     data.members = list(set(data.members))
     group = await chat_group.insert_one(
-        {**data.model_dump(), "created_at": str(datetime.utcnow())}
+        {**data.model_dump(), "created_at": str(datetime.now(timezone.utc))}
     )
     new_group = await chat_group.find_one({"_id": group.inserted_id})
     chat_room.insert_one({
         "room_type": "group",
         "group_id": ObjectId(new_group["_id"]),
         "users": [],
-        "created_at": str(datetime.utcnow())
+        "created_at": str(datetime.now(timezone.utc))
     }) 
     return GroupSerializer(new_group)
 
@@ -98,7 +98,7 @@ async def start_individual_room(
             "room_type": "normal",
             "users": users,
             "group_id": None,
-            "created_at": str(datetime.utcnow())
+            "created_at": str(datetime.now(timezone.utc))
         })
         room = await chat_room.find_one({"_id": inserted_room.inserted_id})
     return RoomSerializer(room)
@@ -172,9 +172,9 @@ async def connect_room(
                 "room_id": ObjectId(room_id),
                 "user_id": user_id,
                 "message": data,
-                "created_at": str(datetime.utcnow())
+                "created_at": str(datetime.now(timezone.utc))
             })
-            broadcasting =  socket.broadcast(f"{user_id}: {data}")
+            broadcasting = socket.broadcast(f"{user_id}: {data}")
             await asyncio.gather(insertion, broadcasting)
     except:
         await socket.disconnect(websocket)
